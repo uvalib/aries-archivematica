96425d3012df1f53391087b1ea2cbf133d26ae63
Jason Jordan <jlj5aj@virginia.edu>
2018-10-12 15:49:42 -0400

    implement master file lookup

--- cmd/aries-archivematica/archivematica.go
+++ cmd/aries-archivematica/archivematica.go
@@ -11,69 +11,9 @@ import (
 	"github.com/satori/go.uuid"
 )
 
-func getAIPsViaApplication() {
-	qs := "select sipUUID, aipFilename from SIPs where hidden = 0 and aipFilename is not null"
-
-	rows, err := adb.Query(qs)
-
-	if err != nil {
-		logger.Printf("adb.Query() failed: [%s]", err.Error())
-		return
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-		var sipUUID string
-		var aipFilename string
-		err = rows.Scan(&sipUUID, &aipFilename)
-		if err != nil {
-			logger.Printf("rows.Scan() failed: [%s]", err.Error())
-			return
-		}
-		logger.Printf("sipUUID: [%s]  aipFilename: [%s]", sipUUID, aipFilename)
-	}
-
-	// get any error encountered during iteration
-	err = rows.Err()
-	if err != nil {
-		logger.Printf("rows.Err() failed: [%s]", err.Error())
-		return
-	}
-}
-
-func getAIPsViaStorageService() {
-	qs := `select p.uuid, s.path || l.relative_path || '/' || p.current_path as fullpath from locations_package p left join locations_location l on p.current_location_id = l.uuid left join locations_space s on l.space_id = s.uuid where l.enabled = 1 and l.purpose = "AS" and p.package_type = "AIP" and p.status = "UPLOADED"`
-
-	rows, err := sdb.Query(qs)
-
-	if err != nil {
-		logger.Printf("sdb.Query() failed: [%s]", err.Error())
-		return
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-		var uuid string
-		var path string
-		err = rows.Scan(&uuid, &path)
-		if err != nil {
-			logger.Printf("rows.Scan() failed: [%s]", err.Error())
-			return
-		}
-		logger.Printf("uuid: [%s]  path: [%s]", uuid, path)
-	}
-
-	// get any error encountered during iteration
-	err = rows.Err()
-	if err != nil {
-		logger.Printf("rows.Err() failed: [%s]", err.Error())
-		return
-	}
-}
-
-func getAIPInfoFromSIPTable(whereClause string) (string, string, error) {
+// retrieves and extracts the AIP UUID and name based on UUID or name (via where clause),
+// using values in the application database
+func getUUIDAndNameFromApplication(whereClause string) (string, string, error) {
 	qs := fmt.Sprintf(`select sipUUID, aipFilename from SIPs where hidden = 0 and %s`, whereClause)
 
 	logger.Printf("query: [%s]", qs)
@@ -81,7 +21,7 @@ func getAIPInfoFromSIPTable(whereClause string) (string, string, error) {
 	rows, err := adb.Query(qs)
 
 	if err != nil {
-		logger.Printf("[getAIPInfoFromSIPTable] adb.Query() failed: [%s]", err.Error())
+		logger.Printf("[getUUIDAndNameFromApplication] adb.Query() failed: [%s]", err.Error())
 		return "", "", errors.New("AIP from SIP table lookup query failed")
 	}
 
@@ -94,7 +34,7 @@ func getAIPInfoFromSIPTable(whereClause string) (string, string, error) {
 		var aipFilename string
 		err = rows.Scan(&sipUUID, &aipFilename)
 		if err != nil {
-			logger.Printf("[getAIPInfoFromSIPTable] rows.Scan() failed: [%s]", err.Error())
+			logger.Printf("[getUUIDAndNameFromApplication] rows.Scan() failed: [%s]", err.Error())
 			return "", "", errors.New("AIP from SIP table results scanning failed")
 		}
 		logger.Printf("sipUUID: [%s]  aipFilename: [%s]", sipUUID, aipFilename)
@@ -117,25 +57,60 @@ func getAIPInfoFromSIPTable(whereClause string) (string, string, error) {
 
 	switch {
 	case cnt == 0:
-		logger.Printf("[getAIPInfoFromSIPTable] no results")
+		logger.Printf("[getUUIDAndNameFromApplication] no results")
 		return "", "", errors.New("AIP from SIP table query returned no results")
 	case cnt > 1:
-		logger.Printf("[getAIPInfoFromSIPTable] too many results")
+		logger.Printf("[getUUIDAndNameFromApplication] too many results")
 		return "", "", errors.New("AIP from SIP table query returned too many results")
 	}
 
 	return uuids[0], names[0], nil
 }
 
-func getFileFromUUID(aipUUID string) (string, error) {
-	return "fakeFile", nil
+// builds the full path to the master file for the given AIP UUID,
+// using values in the storage service database
+func getMasterFileFromStorageService(aipUUID string) (string, error) {
+	qs := fmt.Sprintf(`select s.path || l.relative_path || '/' || p.current_path as file from locations_package p left join locations_location l on p.current_location_id = l.uuid left join locations_space s on l.space_id = s.uuid where l.enabled = 1 and l.purpose = "AS" and p.package_type = "AIP" and p.status = "UPLOADED" and p.uuid = '%s'`, aipUUID)
+
+	rows, err := sdb.Query(qs)
+
+	if err != nil {
+		logger.Printf("[getMasterFileFromStorageService] sdb.Query() failed: [%s]", err.Error())
+		return "", errors.New("Master file lookup query failed")
+	}
+
+	defer rows.Close()
+
+	var files []string
+
+	for rows.Next() {
+		var file string
+		err = rows.Scan(&file)
+		if err != nil {
+			logger.Printf("[getMasterFileFromStorageService] rows.Scan() failed: [%s]", err.Error())
+			return "", errors.New("Master file lookup query results scanning failed")
+		}
+		logger.Printf("file: [%s]", file)
+
+		files = append(files, file)
+	}
+
+	cnt := len(files)
+
+	switch {
+	case cnt == 0:
+		logger.Printf("[getMasterFileFromStorageService] no results")
+		return "", errors.New("Master file lookup query returned no results")
+	case cnt > 1:
+		logger.Printf("[getMasterFileFromStorageService] too many results")
+		return "", errors.New("Master file lookup query returned too many results")
+	}
+
+	return files[0], nil
 }
 
+// takes an ID (name or UUID) and retrieves name/UUID/filename for the matching AIP, if any
 func getAIPFromId(id string) (*AriesAPI, error) {
-	// 1. if id is a UUID, lookup AIP filename in adb; otherwise, lookup UUID
-	// 2. extract AIP name from AIP filename: ${AIPName}-${UUID}.7z
-	// 2. lookup AIP location in sdb based on UUID
-
 	var aipUUID, aipName, aipFile, where string
 	var aipErr error
 	var aipInfo AriesAPI
@@ -151,14 +126,14 @@ func getAIPFromId(id string) (*AriesAPI, error) {
 		where = fmt.Sprintf(`lcase(sipUUID) = lcase('%s')`, uuid.String())
 	}
 
-	aipUUID, aipName, aipErr = getAIPInfoFromSIPTable(where)
+	aipUUID, aipName, aipErr = getUUIDAndNameFromApplication(where)
 
 	if aipErr != nil {
 		logger.Printf("AIP identifier lookup failed: %s", aipErr.Error())
 		return nil, errors.New("AIP identifier lookup failed")
 	}
 
-	aipFile, aipErr = getFileFromUUID(aipUUID)
+	aipFile, aipErr = getMasterFileFromStorageService(aipUUID)
 	if aipErr != nil {
 		logger.Printf("AIP filename lookup failed: %s", aipErr.Error())
 		return nil, errors.New("AIP filename lookup failed")
